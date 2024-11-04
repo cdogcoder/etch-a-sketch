a84d167a977bc0881ca926c5f21563fc97566397
cdogcoder <cdogcoder@gmail.com>
2024-11-03 18:47:49 -0800

    Add paragraph to understand the problem in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Understand the problem
+// The task is to create an Etch-a-Sketch
+// type program (really just a whiteboard)
+// where the user can use their cursor to
+// draw things on a canvas. (Using Odin's
+// requirements) The canvas should be a
+// grid of empty squares (each of the squares
+// are like pixels on a screen essentially)
+// and upon hovering over any one of them 
+// with the cursor, the square gets filled
+// in. The user should also be able to determine
+// the size of the canvas, or how many squares 
+// the width and the length the canvas should be
+// (ex. if the user picks 15 as the size, the
+// generated canvas will have a width and length
+// of 15 squares). The creation of the canvas and
+// the "square fill" effect when hovering over the
+// squares should be done through DOM manipulation.
